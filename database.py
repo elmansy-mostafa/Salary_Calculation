1f410e7d786fbff21f70ecf8674054fef6a09205
Mostafa Elmansy <mostafaelmansy981125@gmail.com>
2024-07-28 23:16:41 +0300

    create database.py

--- database.py
+++ database.py
@@ -1,2 +1,8 @@
-# from motor.motor_asyncio import AsyncIOMotorClient
-# from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+from models import DailyReport, Employee
+
+async def init_db():
+    client = AsyncIOMotorClient("monogodb://localhost:27017")
+    db = client.telesales
+    await init_beanie(database=db, document_models=[DailyReport, Employee])
